3077d599cf6b82e520d245b94c554a1f88dcc157
Олег Малютин <91494915+Le-Ga@users.noreply.github.com>
2022-07-05 13:02:24 +0300

    Update Header.jsx

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -3,12 +3,12 @@ function Header() {
     <div>
       <nav className="green darken-1">
         <div className="nav-wrapper">
-          <a href="!#" className="brand-logo">
+          <a href="/" className="brand-logo">
             React Movies
           </a>
           <ul id="nav-mobile" className="right hide-on-med-and-down">
             <li>
-              <a href="!#">Repo</a>
+              <a href="https://github.com/Le-Ga/react-movies" target="_blank" rel="noreferrer">Repo</a>
             </li>
           </ul>
         </div>
